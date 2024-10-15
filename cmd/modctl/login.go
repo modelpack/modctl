178b8328a686e68daa98750c838a63f3023b949e
Gaius <gaius.qi@gmail.com>
2024-10-15 18:54:54 +0800

    feat: initialize project
    Signed-off-by: Gaius <gaius.qi@gmail.com>

---
+++ cmd/modctl/login.go
@@ -0,0 +1,55 @@
+/*
+ *     Copyright 2024 The CNAI Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package modctl
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// loginCmd represents the modctl command for login.
+var loginCmd = &cobra.Command{
+	Use:                "login [flags]",
+	Short:              "A command line tool for modctl login",
+	Args:               cobra.NoArgs,
+	DisableAutoGenTag:  true,
+	SilenceUsage:       true,
+	FParseErrWhitelist: cobra.FParseErrWhitelist{UnknownFlags: true},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		logrus.Infof("running login")
+		return runLogin(context.Background())
+	},
+}
+
+// init initializes login command.
+func init() {
+	flags := loginCmd.Flags()
+
+	if err := viper.BindPFlags(flags); err != nil {
+		panic(fmt.Errorf("bind cache login flags to viper: %w", err))
+	}
+}
+
+// runLogin runs the login modctl.
+func runLogin(ctx context.Context) error {
+	// TODO: Add login modctl logic here.
+	return nil
+}
---
+++ cmd/modctl/root.go
@@ -0,0 +1,66 @@
+/*
+ *     Copyright 2024 The CNAI Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package modctl
+
+import (
+	"os"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// rootCmd represents the modctl command.
+var rootCmd = &cobra.Command{
+	Use:                "modctl",
+	Short:              "A command line tool for managing artifact bundled based on the Model Format Specification",
+	Args:               cobra.ExactArgs(1),
+	DisableAutoGenTag:  true,
+	SilenceUsage:       true,
+	FParseErrWhitelist: cobra.FParseErrWhitelist{UnknownFlags: true},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		logrus.Debug("modctl is running")
+
+		return nil
+	},
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Bind more cache specific persistent flags.
+	flags := rootCmd.PersistentFlags()
+
+	// Bind common flags.
+	if err := viper.BindPFlags(flags); err != nil {
+		panic(err)
+	}
+
+	// Add sub command.
+	rootCmd.AddCommand(listCmd)
+	rootCmd.AddCommand(loginCmd)
+	rootCmd.AddCommand(logoutCmd)
+	rootCmd.AddCommand(pullCmd)
+	rootCmd.AddCommand(pushCmd)
+	rootCmd.AddCommand(rmCmd)
+}
