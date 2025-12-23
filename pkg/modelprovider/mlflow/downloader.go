cee3ea22294df9b9ed75120428df14a532ffe195
Arsen Gumin <gumin@live.ru>
2025-12-23 19:14:27 +0300

    feature: [WIP] add mlflow support for models://name/version and model/version format
    Signed-off-by: Arsen Gumin <gumin@live.ru>
    
    # Conflicts:
    #	go.sum

---
+++ pkg/modelprovider/mlflow/downloader.go
@@ -0,0 +1,209 @@
+package mlflow
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+	"path/filepath"
+	"slices"
+	"strings"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	awsconfig "github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/databricks/databricks-sdk-go/client"
+	"github.com/databricks/databricks-sdk-go/config"
+	"github.com/databricks/databricks-sdk-go/service/ml"
+)
+
+type MlFlowClient struct {
+	registry *ml.ModelRegistryAPI
+}
+
+// TODO hdfs
+// TODO sql
+// TODO file
+// Read https://mlflow.org/docs/latest/self-hosting/architecture/backend-store/
+var (
+	storageProvider = map[string]StorageBackend{
+		"s3": &S3StorageBackend{},
+	}
+)
+
+func NewMlFlowRegistry(mlflowClient *client.DatabricksClient) (MlFlowClient, error) {
+	var registry *ml.ModelRegistryAPI
+
+	if mlflowClient != nil {
+		registry = ml.NewModelRegistry(mlflowClient)
+		fmt.Println("Use default mlflow client for MlFlowRegistryAPI")
+		return MlFlowClient{registry: registry}, nil
+	}
+
+	//TODO Support more auth methods?
+	cfg := config.Config{
+		//Credentials: config.BasicCredentials{},
+	}
+	mlClient, err := client.New(&cfg)
+
+	if err != nil {
+		return MlFlowClient{}, err
+	}
+	registry = ml.NewModelRegistry(mlClient)
+	return MlFlowClient{registry: registry}, nil
+}
+
+func (mlfr *MlFlowClient) PullModelByName(ctx context.Context, modelName string, modelVersion string, destSrc string) error {
+
+	if mlfr == nil || mlfr.registry == nil {
+		return errors.New("Mlflow client is not initialized: registry is nil")
+	}
+
+	versions, err := mlfr.registry.GetLatestVersionsAll(ctx, ml.GetLatestVersionsRequest{Name: modelName})
+	if err != nil {
+		return errors.Join(errors.New(fmt.Sprintf("failed to get versions for model: %s", modelName)), err)
+	}
+
+	if len(versions) == 0 {
+		return errors.New(fmt.Sprintf("model %s has versions: %v", modelName, versions))
+	}
+
+	var rawVersion []string
+	for _, version := range versions {
+		rawVersion = append(rawVersion, version.Version)
+	}
+	contains := slices.Contains(rawVersion, modelVersion)
+	if !contains {
+		msg := fmt.Sprintf(
+			"model %s version %s not found, available version %v", modelName, modelVersion, rawVersion,
+		)
+		return errors.New(msg)
+	}
+
+	fmt.Printf("Found versions: '%v' for model '%s'\n", rawVersion, modelName)
+	if modelVersion == "" {
+		modelVersion = rawVersion[0]
+	}
+
+	uri, err := mlfr.registry.GetModelVersionDownloadUri(ctx, ml.GetModelVersionDownloadUriRequest{
+		Name:    modelName,
+		Version: modelVersion,
+	})
+
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Try pull model from uri %s", uri.ArtifactUri)
+	parsed, err := url.Parse(uri.ArtifactUri)
+	if parsed == nil {
+		return errors.New("failed to parse artifact uri")
+	}
+
+	switch parsed.Scheme {
+	case "s3":
+		s3storage := storageProvider[parsed.Scheme]
+		err = s3storage.DownloadModel(ctx, uri.ArtifactUri+"/", destSrc) // it's dir
+		if err != nil {
+			return err
+		}
+	default:
+		msg := fmt.Sprintf("Unsupported artifact storage type: %s", parsed.Scheme)
+		err = errors.New(msg)
+		return err
+	}
+
+	fmt.Printf("✅ Model downloaded")
+
+	return nil
+}
+
+type S3StorageBackend struct {
+	addressing string
+}
+
+func (s3back *S3StorageBackend) DownloadModel(ctx context.Context, path string, destPath string) error {
+
+	parsed, err := url.Parse(path)
+	if err != nil {
+		return err
+	}
+
+	bucketName := parsed.Host
+	s3FolderPrefix := parsed.Path[1:]
+	fmt.Printf("Parsed s3 bucket %s, path %s from path", bucketName, s3FolderPrefix)
+	if destPath == "" {
+		destPath = "./downloads/"
+	}
+	cfg, err := awsconfig.LoadDefaultConfig(ctx)
+	if err != nil {
+		wrap := errors.New(fmt.Sprintf("Error loading AWS config, try change envs or profile: %v\n", err))
+		return errors.Join(wrap, err)
+	}
+
+	log.Printf("Region - %s, endpoint - %s", cfg.Region, aws.ToString(cfg.BaseEndpoint))
+
+	s3Client := s3.NewFromConfig(cfg)
+
+	var partMiBs int64 = 10
+	downloader := manager.NewDownloader(s3Client, func(d *manager.Downloader) {
+		d.PartSize = partMiBs * 1024 * 1024
+	})
+	// List objects with the specified prefix
+	paginator := s3.NewListObjectsV2Paginator(s3Client, &s3.ListObjectsV2Input{
+		Bucket:    aws.String(bucketName),
+		Prefix:    aws.String(s3FolderPrefix),
+		Delimiter: aws.String("/"),
+	})
+
+	log.Printf("Start downloading from s3 bucket %s, path %s", bucketName, s3FolderPrefix)
+
+	for paginator.HasMorePages() {
+		page, err := paginator.NextPage(ctx)
+		if err != nil {
+			log.Printf("Error listing objects: %v\n", err)
+			return err
+		}
+
+		for _, object := range page.Contents {
+			s3Key := *object.Key
+			log.Printf("Downloading object: %s\n", s3Key)
+			//if strings.HasSuffix(s3Key, "/") { // Skip S3 "folder" markers
+			//	continue
+			//}
+
+			// Construct local file path
+			relativePath := strings.TrimPrefix(s3Key, s3FolderPrefix)
+			localFilePath := filepath.Join(destPath, relativePath)
+
+			// Create local directories if they don't exist
+			err = os.MkdirAll(filepath.Dir(localFilePath), 0755)
+			if err != nil {
+				log.Printf("Error creating local directory %s: %v\n", filepath.Dir(localFilePath), err)
+				continue
+			}
+
+			// Download the object
+			file, err := os.Create(localFilePath)
+			if err != nil {
+				log.Printf("Error creating local file %s: %v\n", localFilePath, err)
+				continue
+			}
+			defer file.Close()
+
+			numBytes, err := downloader.Download(ctx, file, &s3.GetObjectInput{
+				Bucket: aws.String(bucketName),
+				Key:    aws.String(s3Key),
+			})
+			if err != nil {
+				log.Printf("Error downloading object %s: %v\n", s3Key, err)
+				continue
+			}
+			log.Printf("Downloaded %s to %s (%d bytes)\n", s3Key, localFilePath, numBytes)
+		}
+	}
+
+	return nil
+}
---
+++ pkg/modelprovider/mlflow/provider.go
@@ -0,0 +1,119 @@
+/*
+ *     Copyright 2025 The CNAI Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package mlflow
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+// MlflowProvider implements the modelprovider.Provider interface for Mlflow
+type MlflowProvider struct {
+	mlfClient MlFlowClient
+}
+
+// New creates a new ModelScope provider instance
+func New() *MlflowProvider {
+	return &MlflowProvider{}
+}
+
+// Name returns the name of this provider
+func (p *MlflowProvider) Name() string {
+	return "mlflow"
+}
+
+// SupportsURL checks if this provider can handle the given URL
+// Supports experiments url (runs), model-registry(models)
+// For short-form repo identifiers (owner/repo), users must explicitly specify --provider mlflow
+func (p *MlflowProvider) SupportsURL(url string) bool {
+	url = strings.TrimSpace(url)
+	// TODO Mlflow API equals with Databricks Model Registry, support later
+	possibleUrls := []string{"models", "runs"}
+
+	return hasAnyPrefix(url, possibleUrls)
+}
+
+// DownloadModel downloads a model from ModelScope using the modelscope CLI
+func (p *MlflowProvider) DownloadModel(ctx context.Context, modelURL, destDir string) (string, error) {
+	model, version, err := parseModelURL(modelURL)
+	xclient := NewMlFlowRegistry(nil)
+	xclient.PullModelByName(modelURL)
+	return downloadPath, nil
+}
+
+// CheckAuth verifies that the user is authenticated with MlFlow
+func (p *MlflowProvider) CheckAuth() error {
+	return checkMlflowAuth()
+}
+
+func hasAnyPrefix(s string, subs []string) bool {
+	for _, sub := range subs {
+		if strings.HasPrefix(s, sub) { // Check if the main string contains the current substring
+			return true
+		}
+	}
+	return false
+}
+
+func checkMlflowAuth() error {
+	var err error
+
+	host := os.Getenv("DATABRICKS_HOST")
+	usr := os.Getenv("DATABRICKS_USERNAME")
+	pass := os.Getenv("DATABRICKS_PASSWORD")
+	mlfhost := os.Getenv("MLFLOW_TRACKING_URI")
+	mlfuser := os.Getenv("MLFLOW_TRACKING_USERNAME")
+	mlfpass := os.Getenv("MLFLOW_TRACKING_PASSWORD")
+
+	if host == "" && usr == "" && pass == "" {
+		fmt.Println("Please set DATABRICKS_HOST environment variable.")
+		fmt.Println("Please set DATABRICKS_USERNAME environment variable.")
+		fmt.Println("Please set DATABRICKS_PASSWORD environment variable.")
+	} else if mlfhost != "" && mlfuser != "" && mlfpass != "" {
+		err = os.Setenv("DATABRICKS_HOST", mlfhost)
+		if err != nil {
+			return err
+		}
+		err = os.Setenv("DATABRICKS_USERNAME", usr)
+		if err != nil {
+			return err
+		}
+		err = os.Setenv("DATABRICKS_PASSWORD", pass)
+		if err != nil {
+			return err
+		}
+
+	} else {
+		return errors.New("please set MLFLOW tracking environment variable.")
+	}
+	return err
+}
+
+func parseModelURL(modelURL string) (name, version, error) {
+	var err error
+	var name string
+	var version string
+
+	name = ""
+	version = ""
+	return
+}
